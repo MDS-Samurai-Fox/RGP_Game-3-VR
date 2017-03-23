ba3090851861b0664f1045f4aa2979a056dfe445
Juan Rodríguez <timrodz@icloud.com>
2017-03-23 13:02:32 +1300

    Remove unused assets, add packages, create folders, create ModelController script

--- Assets/Scripts/_UI/AppearAfterSeconds.cs
+++ Assets/Scripts/_UI/AppearAfterSeconds.cs
@@ -1,49 +1,50 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
 
-[RequireComponent (typeof(CanvasGroup))]
+
+[RequireComponent(typeof (CanvasGroup))]
 public class AppearAfterSeconds : MonoBehaviour {
 
-	/// <summary>
-	/// The seconds after which the object will appear
-	/// </summary>
-	public float SecondsToWait = 1f;
+    /// <summary>
+    /// The seconds after which the object will appear
+    /// </summary>
+    public float SecondsToWait = 1f;
 
-	/// <summary>
-	/// Whether or not the object should fade upwards
-	/// </summary>
-	public bool shouldFadeUpwards = true;
+    /// <summary>
+    /// Whether or not the object should fade upwards
+    /// </summary>
+    public bool shouldFadeUpwards = true;
 
-	/// <summary>
-	/// The amount to move the object by
-	/// </summary>
-	public float moveAmount = 0.2f;
+    /// <summary>
+    /// The amount to move the object by
+    /// </summary>
+    public float moveAmount = 0.2f;
 
-	private CanvasGroup cv;
+    private CanvasGroup cv;
 
-	void Awake() {
+    void Awake() {
 
-		cv = GetComponent<CanvasGroup>();
+        cv = GetComponent<CanvasGroup>();
 
-	}
+    }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
 
-		cv.alpha = 0;
+        cv.alpha = 0;
 
-		Invoke("FadeIn", SecondsToWait);
-	
-	}
+        Invoke("FadeIn", SecondsToWait);
 
-	void FadeIn() {
+    }
 
-		if (!shouldFadeUpwards)
-			moveAmount *= -1;
+    void FadeIn() {
 
-		StartCoroutine(UIAnimation.FadeIn(cv, 1));
-		iTween.MoveBy(cv.gameObject, iTween.Hash("y", moveAmount, "easeType", "easeInOutCubic"));
+        if (!shouldFadeUpwards)
+            moveAmount *= -1;
 
-	}
+        StartCoroutine(UIAnimation.FadeIn(cv, 1));
+
+    }
 
 }
